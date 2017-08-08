c45feeeddf323e1f44a50c3bbc239487e2feb465
Thomas Hazledine <tom.hazledine@headforwards.com>
2017-08-08 15:04:47 +0100

    basic boilerplate files

---
+++ uncompressed/js/data.js
@@ -0,0 +1,95 @@
+var data = [
+    {
+        "class_name": "dashboard",
+        "href": "/cmp/dashboard",
+        "title": "Dashboard"
+    }, {
+        "href": null,
+        "menu": [{
+            "href": "/cmp/",
+            "title": "Overview"
+        }, {
+            "href": "/cmp/resources/",
+            "title": "All"
+        }, {
+            "href": "/cmp/resources/#servers",
+            "title": "Servers"
+        }, {
+            "href": "/cmp/resources/#networks",
+            "title": "Network"
+        }, {
+            "href": "/cmp/resources/#storage",
+            "title": "Storage"
+        }, {
+            "href": "/cmp/resources/#compute-pools",
+            "title": "Compute Pools"
+        }, {
+            "href": "/cmp/resources/#appliances",
+            "title": "Appliances"
+        }],
+        "title": "Resources"
+    }, {
+        "href": null,
+        "menu": [{
+            "href": "/cmp/logs/",
+            "title": "Logs"
+        }],
+        "title": "Reporting"
+    }, {
+        "href": null,
+        "menu": [{
+            "href": "/cmp/resources/#accounts",
+            "title": "Accounts"
+        }, {
+            "href": "/cmp/resources/#applications",
+            "title": "Applications"
+        }, {
+            "href": "/cmp/resources/#departments",
+            "title": "Departments"
+        }, {
+            "href": "/cmp/access-control/",
+            "title": "Access Control"
+        }, {
+            "href": "/cmp/metadata/",
+            "title": "Metadata"
+        }],
+        "title": "Admin"
+    }, {
+        "href": null,
+        "menu": [{
+            "href": "/cmp/op/identity",
+            "title": "Identity"
+        }, {
+            "href": "/cmp/op/policy",
+            "title": "Policy"
+        }, {
+            "href": "/cmp/op/spend",
+            "title": "Spend"
+        }, {
+            "href": "/cmp/op/logs",
+            "title": "Audit"
+        }, {
+            "href": "/cmp/op/status",
+            "title": "Status"
+        }, {
+            "href": "/cmp/op/babel/customers",
+            "title": "Babel"
+        }, {
+            "href": "/cmp/op/alerts",
+            "title": "Alerts"
+        }, {
+            "href": "/cmp/op/rules",
+            "title": "Alert Rules"
+        }, {
+            "href": "/cmp/op/orbit/messages",
+            "title": "Orbit Messages"
+        }, {
+            "href": "/cmp/op/orbit/templates",
+            "title": "Orbit Templates"
+        }, {
+            "href": "/cmp/op/notifications",
+            "title": "Notifications"
+        }],
+        "title": "Operator"
+    }
+];
